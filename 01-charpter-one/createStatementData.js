ea512450cec27201e4cc0f8f9b55126905e77bcd
João Paulo <joaopaulozangrande@gmail.com>
2025-08-26 14:01:15 -0300

    move function

--- 01-charpter-one/createStatementData.js
+++ 01-charpter-one/createStatementData.js
@@ -9,38 +9,33 @@ export default function createStatementData(invoice,plays) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
-        result.amount = amountFor(result);
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
-    function amountFor(aPerformance) {
+    function amountFor() {
+        return new PerformanceCalculator(aPerformance,playFor(aPerformance)).amount;
         let result = 0;
-        switch (aPerformance.play.type) {
+        switch (this.play.type) {
         case "tragedy":
             result = 40000;
-            if(aPerformance.audience > 30){
-                result += 1000 * (aPerformance.audience - 30);
+            if(this.performance.audience > 30){
+                result += 1000 * (this.performance.audience - 30);
             }
             break;
         case "comedy":
             result = 30000;
-            if(aPerformance.audience > 20){
-                result += 10000 + 500 * (aPerformance.audience - 20);
+            if(this.performance.audience > 20){
+                result += 10000 + 500 * (this.performance.audience - 20);
             }
-            result += 300 * aPerformance.audience;
+            result += 300 * this.performance.audience;
             break;
         default:
-            throw new Error(`unknown type: ${aPerformance.play.type}`);
+            throw new Error(`unknown type: ${this.performance.play.type}`);
         }
-        return result;
-    }
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30,0);
-        if("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
         return result;
     }
     function totalAmount(data) {
@@ -55,5 +50,11 @@ class PerformanceCalculator {
     constructor(aPerformance, aPlay) {
         this.performance = aPerformance;
         this.play = aPlay;
+    }
+    get VolumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30,0);
+        if("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
+        return result;
     }
 }
