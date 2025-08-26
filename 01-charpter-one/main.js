78379d5bdf717d9af652f04353f7badfb4799dd0
João Paulo <joaopaulozangrande@gmail.com>
2025-08-26 10:56:58 -0300

    adding an object

--- 01-charpter-one/main.js
+++ 01-charpter-one/main.js
@@ -5,10 +5,11 @@ const playJSON = data.readFileSync('plays.json','utf8');
 const plays = JSON.parse(playJSON);
 
 function statement (invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+    return renderPlainText(statementData ,invoice, plays);
 }
 
-function renderPlainText() {
+function renderPlainText(statementData, invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for(let perf of invoice.performances) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
